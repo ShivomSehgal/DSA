dadb0f511f1c8f275908c8a5332de1015fac007c
Billionare <70501837+ShivomSehgal@users.noreply.github.com>
2021-01-29 23:23:02 +0530

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,11 @@
+public class Node {
+
+    int key;
+    String value;
+    Node leftchild, rightchild;
+    public Node(int key, String value){
+        super();
+        this.key=key;
+        this.value=value;
+    }
+}
